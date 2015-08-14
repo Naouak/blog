d9e1b654e08bbce5c4980cd93b8d41626f481f04
Naouak <tardiel@gmail.com>
2015-08-14 08:23:24 +0200

    adding admin base route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => env('APP_ADMIN_LOCATION', 'admin'), "middleware" => "auth"], function(){
+	Route::get('/', function () {
+		return view('welcome');
+	});
+});
