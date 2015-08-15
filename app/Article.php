5ccb50c5f901cbcd9aca846250c111bd405d519b
Naouak <tardiel@gmail.com>
2015-08-15 16:48:44 +0200

    Ading some constraints to the Article Model

--- app/Article.php
+++ app/Article.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'excerpt', 'content', 'published_at'];
+	protected $dates = ['published_at'];
 
 	public function user() {
 		return $this->hasOne('blog\User');
