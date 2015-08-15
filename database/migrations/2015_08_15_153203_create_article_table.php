a6332bcac4882fb8349df75c328dde6a8a126164
Naouak <tardiel@gmail.com>
2015-08-15 16:09:24 +0200

    Creating Article model

---
+++ database/migrations/2015_08_15_153203_create_article_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id',false,true);
+            $table->string('url')->unique();
+            $table->text('title');
+            $table->text('excerpt')->nullable();
+            $table->longText('content');
+            $table->dateTime('published_at');
+            $table->timestamps();
+
+	        $table->foreign("user_id")
+		        ->references('id')
+		        ->on('users')
+		        ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('article');
+    }
+}
