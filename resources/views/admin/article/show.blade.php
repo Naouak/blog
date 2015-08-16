92a23107ff8be697bda315a28c459728c9f5ba49
Naouak <tardiel@gmail.com>
2015-08-16 20:21:41 +0200

    Adding a link from show view to edit view

--- resources/views/admin/article/show.blade.php
+++ resources/views/admin/article/show.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <h1>{{ $article->title  }}</h1>
 
+    <a href="{{ action('AdminArticleController@edit',$article->id) }}">Modifier l'article</a>
+
     <article>
         {!! $articleContent !!}
     </article>
