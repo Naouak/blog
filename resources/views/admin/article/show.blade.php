79e90c48a18bc3935a668f4f0d3daac9b80ffde4
Naouak <tardiel@gmail.com>
2015-08-16 21:03:05 +0200

    Adding a link back to list of articles from show view.

--- resources/views/admin/article/show.blade.php
+++ resources/views/admin/article/show.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1>{{ $article->title  }}</h1>
 
+    <a href="{{ action('AdminArticleController@index') }}">Liste des articles</a>
     <a href="{{ action('AdminArticleController@edit',$article->id) }}">Modifier l'article</a>
 
     <article>
