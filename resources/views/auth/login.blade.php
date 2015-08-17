306fad90878d923c00710da4f4039bbf84cc21c6
Naouak <tardiel@gmail.com>
2015-08-17 21:01:02 +0200

    Login form fully designed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,26 +1,27 @@
 @extends("admin.app")
 
 @section("content")
-    {!! Form::open(["action" => 'Auth\AuthController@getLogin']) !!}
+    {!! Form::open(["action" => 'Auth\AuthController@getLogin', "class" => "login-form"]) !!}
+            <h1>Connexion requise</h1>
+            <div>
+                {!! Form::label("email", "Email") !!}{!! Form::email("email", old('email'), ["placeholder" => "azure@diamond.com"]) !!}
+            </div>
 
-        <div>
-            {!! Form::label("email", "Email") !!}
-            {!! Form::email("email", old('email')) !!}
-        </div>
+            <div>
+                {!! Form::label('password', 'Mot de passe') !!}{!! Form::password('password', ["placeholder" => "hunter2"]) !!}
+            </div>
 
-        <div>
-            {!! Form::label('password', 'Mot de passe') !!}
-            {!! Form::password('password') !!}
-        </div>
+            <div>
+                {!! Form::checkbox('remember',1,null,['id' => 'remember']) !!}{!! Form::label('remember', 'Se souvenir de moi') !!}
+            </div>
 
-        <div>
-            {!! Form::checkbox('remember',1,null,['id' => 'remember']) !!}
-            {!! Form::label('remember', 'Se souvenir de moi') !!}
-        </div>
-
-        <div>
-            {!! Form::submit() !!}
-        </div>
+            <div class="submit">
+                {!! Form::submit("Connexion") !!}
+            </div>
 
     {!! Form::close() !!}
+@endsection
+
+@section('style')
+    {!! HTML::style('css/admin/auth.css') !!}
 @endsection
