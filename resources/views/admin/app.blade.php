ae26f9aa21d0064d39aa10ab2d0c81e41c28857b
Naouak <tardiel@gmail.com>
2015-08-17 09:23:44 +0200

    Top bar is a bit better (and you can log out now!)

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -10,6 +10,29 @@
 </head>
 <body>
 
+    @section('nav')
+    <nav class="main-nav">
+        <span class="nav-element">
+            <a href="{{ action("AdminController@index") }}">Dashboard</a>
+        </span>
+        <ul>
+            <li class="nav-element">
+                <a href="{{ action("AdminArticleController@index") }}">
+                    Articles
+                </a>
+            </li>
+            <li class="nav-element">
+                <a href="{{ action("AdminArticleController@create") }}">
+                    Nouvel article
+                </a>
+            </li>
+        </ul>
+        <ul class="sys-nav">
+            <li class="nav-element nav-logout"><a href="{{ action("Auth\AuthController@getLogout") }}">Déconnexion</a></li>
+        </ul>
+    </nav>
+    @show
+
     <div class="content">
         @yield('content')
     </div>
