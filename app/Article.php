3c4dd311832bf9cfffd4303f54b3c20899cd80fc
Naouak <tardiel@gmail.com>
2015-08-22 19:47:03 +0200

    Adding a new attribute "available" on articles to easily know if they are available.

--- app/Article.php
+++ app/Article.php
@@ -2,14 +2,21 @@
 
 namespace blog;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     protected $fillable = ['title', 'excerpt', 'content', 'published_at'];
 	protected $dates = ['published_at'];
+	protected $appends = ["available"];
 
 	public function user() {
 		return $this->hasOne('blog\User');
+	}
+
+	public function getAvailableAttribute(){
+		/** @var $this->published_at Carbon */
+		return $this->published && $this->published_at->isPast();
 	}
 }
